25c8db8ac34beb3b9622ea74dd19dbf74a5949fb
Erik Öberg <erik.oberg@miun.se>
2024-06-13 15:56:37 +0200

    #Fa-27 in progress
    Lagt till nytt returvärde

--- GolfDataAPI/Controllers/HandicapsController.cs
+++ GolfDataAPI/Controllers/HandicapsController.cs
@@ -19,7 +19,7 @@ namespace GolfDataAPI.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "sträng";
         }
 
         // POST api/<HandicapsController>
